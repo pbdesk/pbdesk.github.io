6d6c4f1e9e0381fcc493bc28455376b4e01a362f
PinalBhattLive <Pinal-Bhatt@live.com>
2014-03-31 23:46:42 -0400

    31-Mar-2014

---
+++ assets/ng/common/scripts/FooterApp.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+    var FooterApp = angular.module('FooterApp', []);
+    FooterApp.run(['$q', '$rootScope',
+        function ($q, $rootScope) {
+
+        }]);
+})();
+
+(function () {
+    'use strict';
+
+    var controllerId = 'FooterCtrl';
+
+    // TODO: replace app with your module name
+    angular.module('FooterApp').controller(controllerId,
+        ['$scope', FooterCtrl]);
+
+    function FooterCtrl($scope) {
+
+    }
+})();
+
+angular.element(document).ready(function () {
+
+    var footer = document.createElement('footer');
+    footer.setAttribute('ng-controller', 'FooterCtrl');
+    footer.setAttribute('ng-cloak', '');
+
+    var ngInc = document.createElement('ng-include');
+    ngInc.setAttribute('src', "'/assets/ng/common/views/footer.html'");
+    footer.appendChild(ngInc);
+
+    document.body.appendChild(footer);
+    angular.bootstrap(footer, ['FooterApp']);
+});
---
+++ assets/ng/common/scripts/HeadTagApp.js
@@ -0,0 +1,69 @@
+﻿(function () {
+    'use strict';
+    var PBDeskHeaderApp = angular.module('HeadTagApp', []);
+    PBDeskHeaderApp.run(['$q', '$rootScope',
+        function ($q, $rootScope) {
+
+        }]);
+})();
+
+(function () {
+    'use strict';
+
+    var controllerId = 'HeadTagCtrl';
+
+    // TODO: replace app with your module name
+    angular.module('HeadTagApp').controller(controllerId,
+        ['$scope', '$location', PBDeskHeaderCtrl]);
+
+    function PBDeskHeaderCtrl($scope, $location) {
+        var theme = PBDeskJS.CookieUtils.Read('theme'); 
+        if (!theme) {
+            theme = 'cerulean';
+        }
+        $scope.Theme = theme;
+        //$cookies.theme = theme;
+        PBDeskJS.CookieUtils.Create('theme', theme);
+
+        $scope.PageTitle = 'PBDesk - from the desk of Pinal Bhatt!';
+        $scope.MetaDesc = '';
+        $scope.MetaKeywords = '';
+        var absurl = $location.absUrl().toLowerCase();
+        if (absurl.indexOf('/elearning/') != -1) {
+            $scope.PageTitle = "PBDesk - eLearning";
+        }
+        
+        
+
+        
+    }
+})();
+
+angular.element(document).ready(function () {
+    document.head.setAttribute('ng-controller', 'HeadTagCtrl');
+    document.head.setAttribute('ng-cloak', '');
+    document.head.innerHTML = '<meta charset="utf-8"> \
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> \
+        <meta name="viewport" content="width=device-width, initial-scale=1"> \
+        <meta name="description" content="{{MetaDesc}}"> \
+        <meta name="keywords" content="{{MetaKeywords}}"> \
+        <meta name="author" content="Pinal Bhatt [PBDesk.com]"> \
+        <meta property="fb:admins" content="731775365" />   \
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> \
+        <title>{{PageTitle}}</title> \
+        <link rel="shortcut icon" href="/assets/imgs/pbdesk/favicon.ico">  \
+        <link href="https://plus.google.com/104802217330869674552" rel="publisher" /> \
+        <link href="https://plus.google.com/116495447274977757345?rel=author" rel="author" /> \
+        <!--[if lt IE 9]> \
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> \
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> \
+        <![endif]--> \
+        <link ng-href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/{{Theme}}/bootstrap.min.css" rel="stylesheet" id="bootstrap.css.theme"> \
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet"> \
+        <link href="/assets/css/styles.css">';
+
+
+    
+    angular.bootstrap(document.head, ['HeadTagApp']);
+
+});
