3919f67979270b38064d243a6c53bcf38fbdb138
PinalBhattLive <Pinal-Bhatt@live.com>
2014-05-03 14:51:38 -0400

    Breadcrumb work

--- assets/ng/Tutorials/TutorialsController.js
+++ assets/ng/Tutorials/TutorialsController.js
@@ -12,6 +12,8 @@
         $rootScope.SetActiveNav(sitemapNode.id);
         var sitemapSubNode = Sitemap.eLearning.Tutorials;
         $rootScope.SetPgTitle(sitemapSubNode.pgTitle);
+        $scope.SitemapNode = Sitemap.eLearning.Tutorials;
+        $rootScope.GetBreadcrumb();
         $scope.$on('$routeChangeSuccess', function () {
             $rootScope.SetActiveNav(sitemapNode.id);
         });
