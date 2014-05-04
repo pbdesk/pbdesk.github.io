73b0f69675e310cbe8df2adb17e90007b283d1ed
PinalBhattLive <Pinal-Bhatt@live.com>
2014-05-04 17:21:53 -0400

    sitemap work

--- assets/ng/home/HomeController.js
+++ assets/ng/home/HomeController.js
@@ -7,16 +7,22 @@
 
     // TODO: replace app with your module name
     angular.module(PBDeskGHAppName).controller(controllerId,
-        ['$scope', HomeController]);
+        ['$scope', 'Sitemap', HomeController]);
 
-    function HomeController($scope) {
+    function HomeController($scope, Sitemap) {
         $scope.title = 'HomeController';
         $scope.activate = activate;
+        $scope.Sitemap = Sitemap;
+
+        
+        
 
         function activate() { }
 
         $scope.$on('$routeChangeSuccess', function () {
             $('[id^=navItem]').removeClass('active');
         });
+
+       
     }
 })();
